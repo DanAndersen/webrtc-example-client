c40843b06f6593e3f9c1608507668e4f117d5b17
Daniel Andersen <gadren@gmail.com>
2017-05-30 21:31:17 -0700

    init demo client

---
+++ app.js
@@ -0,0 +1,43 @@
+var https   = require("https");
+var fs      = require("fs");
+
+//
+/// Setup the demo client web server
+
+var express = require('express'),
+  path = require('path'),
+  app = express(),
+  serverPort = parseInt(sy.config.port)
+  clientPort = serverPort - 1;
+
+app.set('port', clientPort);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/');
+app.use(express.static(__dirname + '/assets'));
+//app.use(express.static(__dirname + '/node_modules/symple-client/src'));
+//app.use(express.static(__dirname + '/node_modules/symple-client-player/src'));
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    port: serverPort,
+    peer: {
+      user: 'demo',
+      name: 'Demo User',
+      group: 'public'
+    }
+  });
+});
+
+var webServer = https.createServer(
+  {
+    key : fs.readFileSync("certs/privkey1.pem"),
+    cert : fs.readFileSync("certs/fullchain1.pem"),
+  }, app).listen(app.get('port'), function () {
+  console.log('Web server listening on port ' + app.get('port'));
+});
+
+/*
+app.listen(app.get('port'), function () {
+  console.log('Web server listening on port ' + app.get('port'));
+});
+*/
